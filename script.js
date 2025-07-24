648c8ae1cbca9ec5652a5d9da78586ae77ee7b51
lutwamahurbert <94892649+lutwamahurbert@users.noreply.github.com>
2025-07-24 22:26:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -358,6 +358,29 @@ function animatePark() {
 }
 animatePark();
 
+const CACHE_NAME = 'solarpunk-timer-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('service-worker.js');
 }
